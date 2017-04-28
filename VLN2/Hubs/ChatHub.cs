2f42b61d67e958003775289b01ec5e6d5f0f4746
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-04-28 11:49:40 +0000

    Made improvements to the chat functionality.

--- VLN2/Hubs/ChatHub.cs
+++ VLN2/Hubs/ChatHub.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
 namespace VLN2.Hubs
 {
+    public class ChatUser
+    {
+        public string Name { get; set; }
+        public string ConnectionID { get; set; }
+
+        public ChatUser(string name, string connectionId)
+        {
+            Name = name;
+            ConnectionID = connectionId;
+        }
+    }
+
     public class ChatHub : Hub
     {
+        public static Dictionary<string, ChatUser> ConnectedUsers = new Dictionary<string, ChatUser>();
+
         public void Hello()
         {
-            Clients.All.hello();
+            string name = Context.User.Identity.Name;
+            string id = Context.ConnectionId;
+
+            if (!ConnectedUsers.ContainsKey(id))
+            {
+                ConnectedUsers.Add(id, new ChatUser(name, id));
+
+                Clients.All.userJoinedLobby(name);
+            }
+            System.Diagnostics.Debug.WriteLine("COUNT OF: " + ConnectedUsers.Count);
+            Clients.Caller.joined(ConnectedUsers.Count);
         }
 
         public void Send(string message)
@@ -20,5 +45,15 @@ namespace VLN2.Hubs
             // Call the addNewMessageToPage method to update clients.
             Clients.All.addNewMessageToPage(name, message);
         }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string name = Context.User.Identity.Name;
+            ConnectedUsers.Remove(Context.ConnectionId);
+
+            Clients.All.userLeftLobby(name);
+            return base.OnDisconnected(stopCalled);
+        }
+
     }
 }
--- VLN2/Scripts/Hubs/chat.js
+++ VLN2/Scripts/Hubs/chat.js
@@ -1,24 +1,56 @@
 ﻿$(function () {
+    var numberOfConnectedUsers = 0;
     // Reference the auto-generated proxy for the hub.
     var chat = $.connection.chatHub;
+
+    chat.client.joined = function (connectedUsersCount) {
+        numberOfConnectedUsers = connectedUsersCount;
+        console.log(connectedUsersCount);
+        updateNumberOfConnectedUsers();
+    }
+
     // Create a function that the hub can call back to display messages.
     chat.client.addNewMessageToPage = function (name, message) {
         // Add the message to the page.
-        $('#discussion').append('<li><strong>' + htmlEncode(name)
-            + '</strong>: ' + htmlEncode(message) + '</li>');
+        $('#discussion').append('<li><strong>' + htmlEncode(name) + '</strong>: ' + htmlEncode(message) + '</li>');
     };
+
+    // Gets called when a user joins the lobby
+    chat.client.userJoinedLobby = function (name) {
+        numberOfConnectedUsers++;
+
+        updateNumberOfConnectedUsers();
+        $('#discussion').append('<li><strong>' + htmlEncode(name) + ' Joined the lobby </li>');
+    }
+   
+    // Gets called when a user leaves the lobby
+    chat.client.userLeftLobby = function (name) {
+        numberOfConnectedUsers--;
+
+        updateNumberOfConnectedUsers();
+        $('#discussion').append('<li><strong>' + htmlEncode(name) + ' Left the lobby </li>');
+    }
+
 
     // Set initial focus to message input box.
     $('#message').focus();
     // Start the connection.
     $.connection.hub.start().done(function () {
-        $('#sendmessage').click(function () {
+        chat.server.hello();
+
+        $('#sendmessage').submit(function () {
             // Call the Send method on the hub.
             chat.server.send($('#message').val());
             // Clear text box and reset focus for next comment.
             $('#message').val('').focus();
+
+            return false
         });
     });
+
+    function updateNumberOfConnectedUsers() {
+        $("#number-of-users-connected").html("Users Connected: " + numberOfConnectedUsers);
+    }
 });
 // This optional function html-encodes messages for display in the page.
 function htmlEncode(value) {
