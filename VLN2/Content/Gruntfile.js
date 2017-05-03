667346a68f532170d8be74ba004df4d6b71672c3
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-05-03 14:01:17 +0000

    Added package.json and Gruntfile.js for watching less files.

---
+++ VLN2/Content/Gruntfile.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["less"],
+                    yuicompress: true
+                },
+                files: {
+                    "css/app.css": "less/app.less"
+                }
+            }
+        },
+        watch: {
+            files: "less/*",
+            tasks: ["less"]
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default','watch')
+};
