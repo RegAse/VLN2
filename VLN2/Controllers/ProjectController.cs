fbed76629c204499ef7556c3ea44731e394b0e82
Guðmundur Axel Guðrúnarson <brucejackie21@gmail.com>
2017-05-04 15:51:53 +0000

    Added some test database access ProjectViewModel

--- VLN2/Controllers/ProjectController.cs
+++ VLN2/Controllers/ProjectController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VLN2.Models;
+using VLN2.ViewModels;
 
 namespace VLN2.Controllers
 {
@@ -11,7 +13,10 @@ namespace VLN2.Controllers
         // GET: Project
         public ActionResult Index()
         {
-            return View();
+            ApplicationDbContext db = new ApplicationDbContext();
+            var model = new ProjectViewModel(db.Projects);
+
+            return View(model);
         }
     }
 }
--- VLN2/ViewModels/ProjectViewModel.cs
+++ VLN2/ViewModels/ProjectViewModel.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VLN2.Models;
 
 namespace VLN2.ViewModels
 {
     public class ProjectViewModel
     {
+        public Project Project { get; set; }
+
+        public IEnumerable<Project> Projects { get; set; }
+
+        public ProjectViewModel(IEnumerable<Project> Projects)
+        {
+            this.Projects = Projects;
+        }
+
     }
 }
