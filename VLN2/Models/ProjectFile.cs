9a2e2ad34536d58fc749a82dd8fff324b81f2590
Bobo <brucejackie21@gmail.com>
2017-05-05 16:54:54 +0000

    Added ProjectFile and ProjectRole models to the project folder.

---
+++ VLN2/Models/ProjectFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VLN2.Models
+{
+    public class ProjectFile
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+
+        public virtual Project Project { get; set; }
+    }
+}
--- VLN2/Models/ProjectRole.cs
+++ VLN2/Models/ProjectRole.cs
@@ -5,13 +5,10 @@ using System.Web;
 
 namespace VLN2.Models
 {
-    public class Project
+    public class ProjectRole
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public virtual ICollection<ApplicationUser> ApplicationUsers { get; set; }
-        public virtual ICollection<ProjectFile> ProjectFiles { get; set; }
     }
 }
