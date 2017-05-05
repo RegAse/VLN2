df018cbe1f7d77dbd30f7ecc1c5a1fd74ac2cce7
Alexander <allimon315@gmail.com>
2017-05-05 14:12:06 +0000

    Kept on working on dashboard

--- VLN2/ViewModels/DashboardViewModel.cs
+++ VLN2/ViewModels/DashboardViewModel.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VLN2.Models;
 
 namespace VLN2.ViewModels
 {
     public class DashboardViewModel
     {
+        public string Username;
+
+        IEnumerable<Project> UserProjects;
+
+        DashboardViewModel(string username, IEnumerable<Project> userProjects)
+        {
+            Username = username;
+
+            UserProjects = userProjects;
+        }
     }
 }
