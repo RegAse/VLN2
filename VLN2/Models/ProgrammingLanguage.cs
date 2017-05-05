c3bb38527d5110bf4657f57152bbc91cf5239f40
Bobo <brucejackie21@gmail.com>
2017-05-05 16:45:45 +0000

    Added Template and ProgrammingLanguage Models.

---
+++ VLN2/Models/ProgrammingLanguage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VLN2.Models
+{
+    public class ProgrammingLanguage
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FileExtension { get; set; }
+
+        public virtual ICollection<Template> Templates { get; set; }
+    }
+}
