d6b173bd9a9a83aa6c09e7068b460f44c1cad1ce
Bobo <brucejackie21@gmail.com>
2017-05-07 18:06:25 +0000

    Added a resize drag on the project sidebar.

--- VLN2/Scripts/app.js
+++ VLN2/Scripts/app.js
@@ -1,1 +1,22 @@
-﻿
+﻿var dragging = false;
+$('#dragbar').mousedown(function (e) {
+    e.preventDefault();
+       
+    dragging = true;
+    $(document).mousemove(function (e) {
+        var percentage = (e.pageX / window.innerWidth) * 100;
+        var mainPercentage = 100 - percentage;
+
+        $('#sidebar').css("width", percentage + "%");
+        $('#main').css("width", mainPercentage + "%");
+        $('#ghostbar').remove();
+    });
+});
+
+$(document).mouseup(function (e) {
+    if (dragging) {
+        
+        $(document).unbind('mousemove');
+        dragging = false;
+    }
+});
