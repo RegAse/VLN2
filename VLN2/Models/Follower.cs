6809b5ac489e73b510c3b6ec55c95641f1f06ea8
Arnarinn <arnar105@hotmail.com>
2017-05-09 14:07:16 +0000

    Started follower groundwork

---
+++ VLN2/Models/Follower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VLN2.Models
+{
+    public class Follower
+    {
+        public int ID { get; set; }
+        public int FollowerID { get; set; }
+        public int FollowedID { get; set; }
+    }
+}
---
+++ VLN2/Services/FollowerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VLN2.Models;
+
+namespace VLN2.Services
+{
+    public class FollowerService
+    {
+        private ApplicationDbContext _db;
+
+        public FollowerService()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        /*public Follower GetUserByFollower(string userNameID)
+        {
+            var user = _db.Users.SingleOrDefault(x => x.Description == userNameID);
+
+            return user;
+        }
+        public Follower GetFollowerByUser(string userName)
+        {
+            var user = _db.Users.SingleOrDefault(x => x.UserName == userName);
+
+            return user;
+        }*/
+
+    }
+}
