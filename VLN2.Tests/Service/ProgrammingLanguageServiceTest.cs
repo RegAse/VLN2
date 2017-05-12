0122146ef6e10cb116866e658eed2c68d2dfb682
Alexander <allimon315@gmail.com>
2017-05-12 14:55:58 +0000

    Made test files ready

---
+++ VLN2.Tests/Service/ProgrammingLanguageServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VLN2.Tests.Service
+{
+    [TestClass]
+    public class ProgrammingLanguageServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
