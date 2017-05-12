b1b205a7f7590b7699a85b4aa5f62fb6166af54a
Alexander <allimon315@gmail.com>
2017-05-12 15:30:00 +0000

    Stuff

--- VLN2.Tests/Service/ProjectServiceTest.cs
+++ VLN2.Tests/Service/ProjectServiceTest.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VLN2.Services;
 
 namespace VLN2.Tests.Service
 {
     [TestClass]
     public class ProjectServiceTest
     {
+        ProjectService _service = new ProjectService();
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetProjectByID()
         {
+            int id = 1;
+            var result = _service.GetProjectByID(32);
+            System.Diagnostics.Debug.WriteLine(result.ID);
+            Assert.AreEqual(1, result);
+            
         }
     }
 }
