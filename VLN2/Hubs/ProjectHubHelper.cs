38bd15863aace5c436acb88d428b32a7d073b139
Bobo <brucejackie21@gmail.com>
2017-05-12 13:30:04 +0000

    Commented ProjectHubHelper

--- VLN2/Hubs/ProjectHubHelper.cs
+++ VLN2/Hubs/ProjectHubHelper.cs
@@ -8,6 +8,12 @@ namespace VLN2.Hubs
     public class ProjectHubHelper
     {
 
+        /// <summary>
+        /// Concats the projectID and projectFileID.
+        /// </summary>
+        /// <param name="ProjectID">The ID of the project</param>
+        /// <param name="ProjectFileID">The ID of the project file</param>
+        /// <returns>The Concated string</returns>
         public static string GetLobbyName(int ProjectID, int ProjectFileID)
         {
             return ProjectID + "-" + ProjectFileID;
